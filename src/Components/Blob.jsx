d9bfcfcc9ed3feb571aba06944126eb90bd38216
Matan Dessaur <matandessaur@m8ndoes.com>
2023-06-12 20:45:31 -0400

    Optimization p2

--- src/Components/Blob.jsx
+++ src/Components/Blob.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 
 const colors = ["#4EA5D9", "#EFCA08", "#F87575"];
 
 export default function Blob() {
-    return (
-        <BlobContainer>
-            <InnerBlob />
-            <OuterBlob />
-        </BlobContainer>
-    );
-};
+  const blobRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    if (blobRef.current) {
+      observer.observe(blobRef.current);
+    }
+
+    return () => {
+      if (blobRef.current) {
+        observer.unobserve(blobRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <BlobContainer ref={blobRef} className="blob">
+      <InnerBlob />
+      <OuterBlob />
+    </BlobContainer>
+  );
+}
 
 const liquidAnimation = keyframes`
   0% {
@@ -30,57 +54,63 @@ const liquidAnimation = keyframes`
   }
 `;
 
-
-
 const BlobContainer = styled.div`
-position: absolute;
-z-index: -1;
-filter: blur(40px);
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-width: 500px;
-height: 500px;
-background-color: transparent;
-animation: ${liquidAnimation} 6s cubic-bezier(.78,.23,.2,.77) infinite;
+  position: absolute;
+  z-index: -1;
+  filter: blur(40px);
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 500px;
+  background-color: transparent;
+  animation: ${liquidAnimation} 6s cubic-bezier(.78,.23,.2,.77) infinite;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  visibility: hidden;
 
-&:hover {
- background-color: black;
-}
+  &.show {
+    opacity: 1;
+    visibility: visible;
+  }
 
-@media (max-width: 768px) {
-  width: 400px;
-  height: 400px;
-}
+  &:hover {
+    background-color: black;
+  }
 
-@media (max-width: 480px) {
-  width: 300px;
-  height: 300px;
-}
+  @media (max-width: 768px) {
+    width: 400px;
+    height: 400px;
+  }
 
-@media (max-width: 320px) {
-  width: 200px;
-  height: 200px;
-}
+  @media (max-width: 480px) {
+    width: 300px;
+    height: 300px;
+  }
+
+  @media (max-width: 320px) {
+    width: 200px;
+    height: 200px;
+  }
 `;
 
 const InnerBlob = styled.div`
-position: absolute;
-top: 10px;
-left: 10px;
-right: 10px;
-bottom: 10px;
-border-radius: inherit;
-background: linear-gradient(to bottom right, ${colors[0]}, ${colors[1]}, ${colors[2]});
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  right: 10px;
+  bottom: 10px;
+  border-radius: inherit;
+  background: linear-gradient(to bottom right, ${colors[0]}, ${colors[1]}, ${colors[2]});
 `;
 
 const OuterBlob = styled.div`
-position: absolute;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-border-radius: inherit;
-overflow: hidden;
-background: linear-gradient(to bottom right, ${colors[1]}, ${colors[2]}, ${colors[0]});
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border-radius: inherit;
+  overflow: hidden;
+  background: linear-gradient(to bottom right, ${colors[1]}, ${colors[2]}, ${colors[0]});
 `;
