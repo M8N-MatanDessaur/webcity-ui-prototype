2c472fd1b19e00beee5659f02ff3d587c29fcd8f
Matan Dessaur <matandessaur@m8ndoes.com>
2023-06-21 16:57:46 -0400

    Added automatic language detection

--- src/Translation/i18n.js
+++ src/Translation/i18n.js
@@ -3,22 +3,26 @@ import { initReactI18next } from 'react-i18next';
 import frTranslations from './Fr';
 import enTranslations from './En';
 
-const userPreferredLanguage = navigator.language;
+const initializeI18n = (userPreferredLanguage) => {
+  i18n.use(initReactI18next).init({
+    resources: {
+      fr: {
+        translation: frTranslations,
+      },
+      en: {
+        translation: enTranslations,
+      },
+    },
+    lng: userPreferredLanguage,
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+};
 
-i18n.use(initReactI18next).init({
-  resources: {
-    fr: {
-      translation: frTranslations,
-    },
-    en: {
-      translation: enTranslations,
-    },
-  },
-  lng: userPreferredLanguage,
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+// Example usage: obtain user's preferred language and then initialize i18n
+const userPreferredLanguage = navigator.language;
+initializeI18n(userPreferredLanguage);
 
 export default i18n;
