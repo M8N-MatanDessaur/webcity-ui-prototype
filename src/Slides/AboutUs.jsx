052272057c521803065b5c91092167da8f7bcbb5
Matan Dessaur (M8N) <126172236+M8N-MatanDessaur@users.noreply.github.com>
2023-08-01 19:52:08 +0000

    Small fixes

--- src/Slides/AboutUs.jsx
+++ src/Slides/AboutUs.jsx
@@ -71,13 +71,7 @@ const FluidContainer = styled.div`
   justify-content: center;
   flex-direction: column;
   overflow: hidden;
-  background-image:url(https://uploads-ssl.webflow.com/62e3ee10882dc50bcae8d07a/631a5d4631d4c55a475f3e34_noise-50.png);
-  background-size: 10%;
   background-attachment: fixed;
-
-  @media (max-width: 480px) {
-    background-size: 25%;
-}
 `;
 
 const Separator = styled.hr`
