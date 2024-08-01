b90f513a69030dad4da2d0921982de356cf53969
Matan Dessaur <hello@matandessaur.me>
2024-08-01 11:51:44 -0400

    Refactor TiltContainer background image size for improved performance and readability

--- src/Components/TiltContainer.jsx
+++ src/Components/TiltContainer.jsx
@@ -58,8 +58,6 @@ const Outside = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background-image:url(https://uploads-ssl.webflow.com/62e3ee10882dc50bcae8d07a/631a5d4631d4c55a475f3e34_noise-50.png);
-    background-size: 10%;
 
     @media (max-width: 480px) {
         background-size: 25%;
