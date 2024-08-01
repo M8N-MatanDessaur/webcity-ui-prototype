10c7350e95e73abe243ffce3cb27ccf381355680
Matan Dessaur <hello@matandessaur.me>
2024-08-01 11:11:45 -0400

    feat: Add lazy loading for images in Projects component

--- src/Slides/Projects.jsx
+++ src/Slides/Projects.jsx
@@ -5,10 +5,20 @@ import img1 from "../Assets/Images/media/1.jpg";
 import img2 from "../Assets/Images/media/2.jpg";
 import img3 from "../Assets/Images/media/3.jpg";
 import img4 from "../Assets/Images/media/4.jpg";
+
+import img1fr from "../Assets/Images/media/1fr.jpg";
+import img2fr from "../Assets/Images/media/2fr.jpg";
+import img3fr from "../Assets/Images/media/3fr.jpg";
+import img4fr from "../Assets/Images/media/4fr.jpg";
 import ScrollArrow from "../Components/ScrollArrow";
 
 export default function Projects() {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
+    
+    const isFrench = i18n.language === 'fr';
+
+    const images = isFrench ? [img1fr, img2fr, img3fr, img4fr] : [img1, img2, img3, img4];
+
     return (
         <div id="portfolio" style={{
             padding: "3rem",
@@ -16,33 +26,18 @@ export default function Projects() {
             margin: "0 auto",
             position: "relative",
         }}>
-              <Title>{t('mainText.portfolio')}</Title>
+            <Title>{t('mainText.portfolio')}</Title>
             <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2 }}>
                 <Masonry>
-                    <img
-                        src={img1}
-                        alt={"img1"}
-                        style={{ width: "100%", display: "block", padding: "0.5rem"}}
-                        loading='lazy'
-                    />
-                    <img
-                        src={img2}
-                        alt={"img2"}
-                        style={{ width: "100%", display: "block", padding: "0.5rem"}}
-                        loading='lazy'
-                    />
-                    <img
-                        src={img3}
-                        alt={"img3"}
-                        style={{ width: "100%", display: "block", padding: "0.5rem"}}
-                        loading='lazy'
-                    />
-                    <img
-                        src={img4}
-                        alt={"img4"}
-                        style={{ width: "100%", display: "block", padding: "0.5rem"}}
-                        loading='lazy'
-                    />
+                    {images.map((img, index) => (
+                        <img
+                            key={index}
+                            src={img}
+                            alt={`img${index + 1}`}
+                            style={{ width: "100%", display: "block", padding: "0.5rem"}}
+                            loading='lazy'
+                        />
+                    ))}
                 </Masonry>
             </ResponsiveMasonry>
             <ScrollArrow bottom={"-36px"} />
