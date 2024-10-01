a53961cfc428bef7abfbe41b2d1031e57eaa8730
Matan Dessaur <hello@matandessaur.me>
2024-09-30 21:54:02 -0400

    Refactor index.html to add cookieyes banner

--- src/Components/Toolbox/Toolbox.jsx
+++ src/Components/Toolbox/Toolbox.jsx
@@ -24,7 +24,7 @@ const Categories = [
       },
       {
         "href": "moretools",
-        "svg": "<path fill-rule=\"evenodd\" d=\"M4 8h4V4H4v4Zm6 12h4v-4h-4v4Zm-2 0H4v-4h4v4Zm-4-6h4v-4H4v4Zm10 0h-4v-4h4v4Zm2-10v4h4V4h-4Zm-2 4h-4V4h4v4Zm2 6h4v-4h-4v4Zm4 6h-4v-4h4v4Z\" clip-rule=\"evenodd\"></path>",
+        "svg": "<path fillRule=\"evenodd\" d=\"M4 8h4V4H4v4Zm6 12h4v-4h-4v4Zm-2 0H4v-4h4v4Zm-4-6h4v-4H4v4Zm10 0h-4v-4h4v4Zm2-10v4h4V4h-4Zm-2 4h-4V4h4v4Zm2 6h4v-4h-4v4Zm4 6h-4v-4h4v4Z\" clip-rule=\"evenodd\"></path>",
         "title": "moreTools"
       },
       {
--- src/Components/ToolsListButton/ToolsListButton.jsx
+++ src/Components/ToolsListButton/ToolsListButton.jsx
@@ -13,7 +13,7 @@ export default function ToolsListButton() {
     return (
         <Button onClick={goBack} role="button" title="ToolsList">
             <svg fill="var(--text-color)" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M3 9h4V5H3v4Zm4 5H3v-4h4v4Zm0 5H3v-4h4v4Zm13-5H8v-4h12v4ZM8 19h12v-4H8v4ZM8 9V5h12v4H8Z" clip-rule="evenodd"></path>
+                <path fillRule="evenodd" d="M3 9h4V5H3v4Zm4 5H3v-4h4v4Zm0 5H3v-4h4v4Zm13-5H8v-4h12v4ZM8 19h12v-4H8v4ZM8 9V5h12v4H8Z" clip-rule="evenodd"></path>
             </svg>
         </Button >
     );
